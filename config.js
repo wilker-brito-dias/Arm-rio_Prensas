fc35045d846cc99cef5d87fb5c8c107c1c0d9bfb
Wilker Brito Dias <wilkerwbddias@gmail.com>
2026-09-16 21:42:09 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+// Configuração do Supabase
+const SUPABASE_URL = 'https://lxdmksmufubyuyfxuakh.supabase.co'; 
+const SUPABASE_KEY = 'sb_publishable_CMbXxUECMbtg9h8JEtDjow_bSntFiEi'; // sb_publishable_...
+
+const cliente = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+const SENHA_ADMIN = '7699';
