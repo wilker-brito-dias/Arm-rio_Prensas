b719e63a25c66ffca1eaee2a4102f94c656aa809
Wilker <wilkerwbddias@gmail.com>
2026-09-26 15:50:54 -0300

    Sistema de loguin alterado

--- config.js
+++ config.js
@@ -3,5 +3,3 @@ const SUPABASE_URL = 'https://lxdmksmufubyuyfxuakh.supabase.co';
 const SUPABASE_KEY = 'sb_publishable_CMbXxUECMbtg9h8JEtDjow_bSntFiEi'; // sb_publishable_...
 
 const cliente = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
-
-const SENHA_ADMIN = '7699';
